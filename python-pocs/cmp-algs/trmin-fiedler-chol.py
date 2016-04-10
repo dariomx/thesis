4715f172805cdc3bd1576d2f6cee409a807cd451
dario <dario.bahena@oracle.com>
2016-04-10 10:10:42 -0500

    save work from sat

---
+++ python-pocs/cmp-algs/trmin-fiedler-chol.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from sys import argv
+from fiedler import fiedler_vector
+from util import test
+
+def calc(L):
+    return fiedler_vector(L, method="tracemin_chol")
+
+# main
+if __name__ == '__main__':
+    test(argv, calc)
+    
